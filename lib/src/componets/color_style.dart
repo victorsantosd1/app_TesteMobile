e3e4d29c376d4c8f7870447a03769f87cc2cd979
Victor Santos <victorsantos900@hotmail.com>
2024-05-10 21:16:51 -0300

    cria esquema de cores

---
+++ lib/src/componets/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  static const primary = Color.fromRGBO(29, 29, 27, 1);
+  static const secondary = Color.fromRGBO(20, 200, 113, 1);
+  static const background = 
+}
